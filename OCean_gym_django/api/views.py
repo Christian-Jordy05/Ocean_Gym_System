3beee62b2d29d3cf2b9940cca8500e2e36b3e5cc
Christian-Jordy05 <yordymar224@gmail.com>
2024-09-27 13:28:38 -0600

    mejoracion de diseño para el iniciar sesion, registro y el nav

--- OCean_gym_django/api/views.py
+++ OCean_gym_django/api/views.py
@@ -1,14 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Client  
-from .serializers import ClientSerializer
+from .models import Client, Administrador
+from .serializers import ClientSerializer, AdministradorSerializer
 
+
+
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////7
+# clientes
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def client_detail(request, pk=None):
     if request.method == 'GET':
         if pk:
-            # Obtener un cliente específico
             try:
                 user = Client.objects.get(pk=pk)
                 serializer = ClientSerializer(user)
@@ -16,13 +19,11 @@ def client_detail(request, pk=None):
             except Client.DoesNotExist:
                 return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
         else:
-            # Obtener todos los clientes
             users = Client.objects.all()
             serializer = ClientSerializer(users, many=True)
             return Response(serializer.data)
 
     elif request.method == 'POST':
-        # Crear un nuevo cliente
         serializer = ClientSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -51,3 +52,98 @@ def client_detail(request, pk=None):
                 return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
 
+
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////7
+# adminitradores
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def administrador_detail(request, pk=None):
+    if request.method == 'GET':
+        if pk:
+            try:
+                user = Administrador.objects.get(pk=pk)
+                serializer = AdministradorSerializer(user)
+                return Response(serializer.data)
+            except Administrador.DoesNotExist:
+                return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            users = Administrador.objects.all()
+            serializer = AdministradorSerializer(users, many=True)
+            return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = AdministradorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'PUT':
+        if pk:
+            try:
+                user = Administrador.objects.get(pk=pk)
+                serializer = AdministradorSerializer(user, data=request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            except Administrador.DoesNotExist:
+                return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    elif request.method == 'DELETE':
+        if pk:
+            try:
+                user = Administrador.objects.get(pk=pk)
+                user.delete()
+                return Response({"message": "User deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+            except Administrador.DoesNotExist:
+                return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////7
+# ventas
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def Ventas_detail(request, pk=None):
+    if request.method == 'GET':
+        if pk:
+            try:
+                user = Administrador.objects.get(pk=pk)
+                serializer = AdministradorSerializer(user)
+                return Response(serializer.data)
+            except Administrador.DoesNotExist:
+                return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            users = Administrador.objects.all()
+            serializer = AdministradorSerializer(users, many=True)
+            return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = AdministradorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'PUT':
+        if pk:
+            try:
+                user = Administrador.objects.get(pk=pk)
+                serializer = AdministradorSerializer(user, data=request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            except Administrador.DoesNotExist:
+                return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    elif request.method == 'DELETE':
+        if pk:
+            try:
+                user = Administrador.objects.get(pk=pk)
+                user.delete()
+                return Response({"message": "User deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+            except Administrador.DoesNotExist:
+                return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+
+
+# ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////7
+# NOSE
--- OCean_gym_react/src/Routes/navegar.jsx
+++ OCean_gym_react/src/Routes/navegar.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Nav.css'
+import ocean_gym_transparent from '../componente/img/ocean_gym_transparent.png'
 
 function Navegar() {
   return (
-    <div>
+    <div className='Conteiner_del_nav'>
+
+      {/* LOGO QUE VA EN EL NAV  */}
+      <div className='color_fondo_de_img'>
+      <img className='logo_Nav' src={ocean_gym_transparent} alt="" />
+      </div>
+
+
+      <div className='conte_de_btn_register_iniciar'>
+      {/* BOTON PARA DIRIGIR HOME */}
+      <Link to="/home">
+        <button id='home-btn' type="button" name="home">Home</button>
+      </Link>
+
+
+      {/* BOTON PARA DIRIGIRLO A LOGIN  */}
       <Link to="/login">
-        <button id='login-btn' type="button" name="login">Login</button>
+        <button id='login-btn' type="button" name="login">Iniciar sesion</button>
       </Link>
+
+      {/* BOTON PARA DIRIGIRLO A REGISTER  */}
       <Link to="/sign_up">
-        <button id='signup-btn' type="button" name="signup">Sign Up</button>
+        <button id='signup-btn' type="button" name="signup">Registrarse</button>
       </Link>
+      </div>
+
+
     </div>
   );
 }
