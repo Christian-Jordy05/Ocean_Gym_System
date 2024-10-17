71cfa16c25bb013b91e45a839810b42b435f5b32
Yacith-D <166965191+Yacith-D@users.noreply.github.com>
2024-10-17 08:48:06 -0600

    Se realizo el put y el post donde el administrador podra agregar y editar productos

--- OCean_gym_react/src/pages/PagHome.jsx
+++ OCean_gym_react/src/pages/PagHome.jsx
@@ -1,10 +1,11 @@
 import Home from '../componente/home/Home'
+import Administracion from '../componente/admistracion/Administracion'
 
 function PagHome() {
   return (
     <div>
       <Home/>
-
+      <Administracion/>
     </div>
   )
 }
